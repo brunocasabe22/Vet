7247f58dee505f2e003c794b195ae0804f271436
brunocasabe22 <brunoabecas@gmail.com>
2023-02-20 19:57:13 -0300

    20/0

--- Vet/app/Http/Requests/StoreProductRequest.php
+++ Vet/app/Http/Requests/StoreProductRequest.php
@@ -25,14 +25,12 @@ class StoreProductRequest extends FormRequest
     {
         return [
         'image'=>'required|dimension:min_width=100,min_height=200',
-        'name'=>'required|unique:products|max:255',
-        'sell_price'=>'',
+        'name'=>'string|required|unique:products|max:255',
+        'sell_price'=>'required|',
         'category_id'=>'integer|required|exists:App\User\Category,id',
          'provider_id'=>'integer|required|exists:App\User\Provider,id',
 
-            'name' => 'required|string|max:50',
-            'description' => 'nullable|string|max:250'
-
+           
   
           ];
       }
@@ -42,15 +40,26 @@ class StoreProductRequest extends FormRequest
      
   
       return [
-         'name.required'=>'Este campo es requerido',
-         'name.string'=>'El valor no es correcto',
-          'name.max'=>'Solo se permite 50 caracteres',
+      'name.string'=>'El valor no es correcto',
+      'name.required'=>'El campo es requerido',
+      'name.unique'=>'El producto ya esta registrado',
+      'name.max'=>'Solo se permite 255 caracteres',
 
-  
-         
-         'description.string'=>'El valor no es correcto',
-          'description.max'=>'Solo se permite 255 caracteres',
-          
+      'image.required'=>'El campo es requerido',
+      'image.dimensions'=>'Solo se permiten imagenes de 100x200 px',
+      
+      
+      'sell_price.string'=>'El cambio es requerido',
+      
+
+      'category_id.integer'=>'El valor tiene que ser un entero',
+      'category_id.required'=>'El campo es requerido',
+      'category_id.exists'=>'La categoria no existe',
+
+      'provider_id.integer'=>'El valor tiene que ser un entero',
+      'provider_id.required'=>'El campo es requerido',
+      'provider_id.exists'=>'La categoria no existe',
+    
       ];
   }
 }
--- Vet/app/Http/Requests/UpdateProductRequest.php
+++ Vet/app/Http/Requests/UpdateProductRequest.php
@@ -13,7 +13,7 @@ class UpdateProductRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,8 +23,44 @@ class UpdateProductRequest extends FormRequest
      */
     public function rules()
     {
-        return [
-            //
-        ];
-    }
+        
+            return [
+            'image'=>'required|dimension:min_width=100,min_height=200',
+            'name'=>'string|required|unique:products,name, '.$this->route('product')->id.'|max:255',
+            'sell_price'=>'required|',
+            'category_id'=>'integer|required|exists:App\User\Category,id',
+             'provider_id'=>'integer|required|exists:App\User\Provider,id',
+    
+               
+      
+              ];
+          }
+      
+          public function messages(){
+      
+         
+      
+          return [
+          'name.string'=>'El valor no es correcto',
+          'name.required'=>'El campo es requerido',
+          'name.unique'=>'El producto ya esta registrado',
+          'name.max'=>'Solo se permite 255 caracteres',
+    
+          'image.required'=>'El campo es requerido',
+          'image.dimensions'=>'Solo se permiten imagenes de 100x200 px',
+          
+          
+          'sell_price.string'=>'El cambio es requerido',
+          
+    
+          'category_id.integer'=>'El valor tiene que ser un entero',
+          'category_id.required'=>'El campo es requerido',
+          'category_id.exists'=>'La categoria no existe',
+    
+          'provider_id.integer'=>'El valor tiene que ser un entero',
+          'provider_id.required'=>'El campo es requerido',
+          'provider_id.exists'=>'La categoria no existe',
+        
+          ];
+      }
 }
